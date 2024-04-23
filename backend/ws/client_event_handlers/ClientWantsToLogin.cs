3287f9d54e5e50be597d141ac3b252e0c97cf818
monikabrzezinska05 <114471820+monikabrzezinska05@users.noreply.github.com>
2024-04-23 11:00:04 +0200

    server model og første client

---
+++ backend/ws/client_event_handlers/ClientWantsToLogin.cs
@@ -0,0 +1,35 @@
+namespace ws;
+
+public class ClientWantsToLoginDto : BaseDto
+{
+    public string useremail { get; set; }
+    public string password { get; set; }
+}
+
+public class ClientWantsToLogin : BaseEventHandler<ClientWantsToLoginDto>
+{
+    private readonly UserService _userService;
+    public ClientWantsToLogin(UserService userService)
+    {
+        _userService = userService;
+    }
+    
+    // ændre service ovenover til den, som vi skal bruge!!!
+    
+    public override async Task Handle(ClientWantsToLoginDto dto, IWebSocketConnection socket)
+    {
+        var user = await _userService.Login(dto.usermail, dto.password);
+        if (user == null)
+        {
+            socket.Send("Login failed");
+            return;
+        }
+        var userDto = new ServerBroadcastUserDto()
+        {
+            id = user.Id,
+            username = user.Username,
+            email = user.Email
+        };
+        socket.Send(JsonSerializer.Serialize(userDto));
+    }
+}
