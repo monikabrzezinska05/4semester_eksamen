d4bc9a18f388f1ab8018fdc2a4d77cbe03e1a626
ShadySloth <pedant01@easv365.dk>
2024-04-23 11:02:42 +0200

    setup mqtt
    mangler at sende det til database

--- backend/service/MQTTService.cs
+++ backend/service/MQTTService.cs
@@ -1,6 +1,34 @@
+using infrastructure.repositories;
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Formatter;
+
 namespace service;
 
 public class MQTTService
 {
-    
+    public async Task CommunicateWithBroker(HistoryRepo historyRepo)
+    {
+        var mqttFactory = new MqttFactory();
+        var mqttClient = mqttFactory.CreateMqttClient();
+
+        var mqttClientOptions = new MqttClientOptionsBuilder()
+            .WithTcpServer("localhost", 1883)
+            .WithProtocolVersion(MqttProtocolVersion.V500)
+            .Build();
+
+        await mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
+
+        var mqttSubscribeOptions = mqttFactory.CreateSubscribeOptionsBuilder()
+            .WithTopicFilter(f => f.WithTopic("Security"))
+            .Build();
+
+        await mqttClient.SubscribeAsync(mqttSubscribeOptions, CancellationToken.None);
+
+        mqttClient.ApplicationMessageReceivedAsync += async e =>
+        {
+            var message = e.ApplicationMessage.ConvertPayloadToString();
+            Console.WriteLine("Received message: " + message);
+        };
+    }
 }
