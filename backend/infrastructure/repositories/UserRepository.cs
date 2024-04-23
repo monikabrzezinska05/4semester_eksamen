83df04d61dc3008fd315f99838e00a3c7e6c8fb0
hoeeg79 <93433669+hoeeg79@users.noreply.github.com>
2024-04-23 11:43:44 +0200

    Service and Repo done

--- backend/infrastructure/repositories/UserRepository.cs
+++ backend/infrastructure/repositories/UserRepository.cs
@@ -21,4 +21,34 @@ public class UserRepository
             return conn.QueryFirstOrDefault<User>(sql, new {pwHashUserId});
         }
     }
+    
+    public void CreatePassword(string hash, string salt, int? userId)
+    {
+        var sql = "INSERT INTO public.passwords " +
+                  "(userid, password,salt) " +
+                  "VALUES (@userId, @hash, @salt)";
+        using (var conn = _dataSource.OpenConnection())
+        {
+            conn.Execute(sql, new {userId, hash, salt});
+        }
+    }
+
+    public User CreateUser(User user)
+    {
+        var sql = "INSERT INTO public.\"User\"" +
+                  "(name, mail, child)" +
+                  "VALUES (@name, @mail, @isChild) " +
+                  "ON CONFLICT(mail) DO NOTHING " +
+                  "RETURNING *;";
+
+        using (var conn = _dataSource.OpenConnection())
+        {
+            var response = conn.QueryFirst<User?>(sql, new {user.name, user.email, user.isChild});
+            if (response == null)
+            {
+                throw new Exception("User already exists");
+            }
+            return response;
+        }
+    }
 }
