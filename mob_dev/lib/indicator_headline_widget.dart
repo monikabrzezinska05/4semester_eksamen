cd92e7bccd431711cf275b35ae5629936c6daa20
hoeeg79 <93433669+hoeeg79@users.noreply.github.com>
2024-04-25 10:37:20 +0200

    First designs done

---
+++ mob_dev/lib/indicator_headline_widget.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class IndicatorHeadline extends StatefulWidget {
+  String headline;
+   IndicatorHeadline({
+    required this.headline,
+    super.key,
+  });
+
+  @override
+  State<IndicatorHeadline> createState() => _IndicatorHeadlineState();
+}
+
+class _IndicatorHeadlineState extends State<IndicatorHeadline> {
+  Color TheColor = Colors.red;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 30.0, right: 30.0, top: 40),
+          child: Row(
+            children: [
+              Text(widget.headline, style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+              // Insert whitespace so screen is filled
+              const Spacer(),
+              // Add a circular indicator that can change colour
+              Icon(Icons.circle, color: TheColor)
+            ],
+          ),
+        ),
+        Divider(
+          color: Colors.black,
+          height: 1,
+          thickness: 2,
+          indent: 20,
+          endIndent: 20,
+        )
+      ],
+    );
+  }
+}
---
+++ mob_dev/lib/indicator_lines.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class IndicatorLine extends StatefulWidget {
+  final String unitName;
+  IndicatorLine({
+    required this.unitName,
+    super.key,
+  });
+
+  @override
+  State<IndicatorLine> createState() => _IndicatorLineState();
+}
+
+class _IndicatorLineState extends State<IndicatorLine> {
+  Color TheColor = Colors.red;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+      child: TextButton(
+        style: ButtonStyle(
+          visualDensity: VisualDensity.compact,
+        ),
+        onPressed: () {
+          setState(() {
+            if (TheColor == Colors.red)
+              {
+                TheColor = Colors.green;
+              }
+            else
+              {
+                TheColor = Colors.red;
+              }
+          });
+        },
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 15.0, right: 15.0),
+              child: Row(
+                children: [
+                  Text(widget.unitName, style: TextStyle(fontSize: 14)),
+                  // Insert whitespace so screen is filled
+                  const Spacer(),
+                  // Add a circular indicator that can change colour
+                  Icon(Icons.circle, color: TheColor, size: 15)
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
