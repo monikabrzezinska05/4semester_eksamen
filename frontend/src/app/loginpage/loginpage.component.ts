fa651c3f45f95fc8bda2c0a125ee15e1fb440746
monikabrzezinska05 <114471820+monikabrzezinska05@users.noreply.github.com>
2024-04-30 13:20:12 +0200

    crying and screaming

---
+++ frontend/src/app/loginpage/loginpage.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import {CommonModule} from "@angular/common";
+import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+
+@Component({
+  selector: 'app-loginpage',
+  imports: [CommonModule, ReactiveFormsModule, HttpClientModule],
+  templateUrl: './loginpage.component.html',
+  styleUrls: ['./loginpage.component.css'],
+  standalone: true
+})
+
+export class LoginpageComponent {
+
+  constructor(public http: HttpClientModule,
+              private readonly formBuilder: FormBuilder){}
+
+  readonly login = this.formBuilder.group({
+    Email: ['', [Validators.required, Validators.email]],
+    Password: ['', Validators.required],
+    RememberMe: [false]
+  });
+}
