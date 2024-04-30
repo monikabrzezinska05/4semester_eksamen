dacad1690bc9265644c87207ffc487d87478826e
monikabrzezinska05 <114471820+monikabrzezinska05@users.noreply.github.com>
2024-04-30 09:25:13 +0200

    nope

--- backend/ws/client_event_handlers/ClientWantsToLogoff.cs
+++ backend/ws/client_event_handlers/ClientWantsToLogoff.cs
@@ -6,13 +6,14 @@ namespace ws;
 
 public class ClientWantsToLogoffDto : BaseDto
 {
-    
+    public string email { get; set; }
+    public string password { get; set; }
 }
 
 public class ClientWantsToLogoff : BaseEventHandler<ClientWantsToLogoffDto>
 {
     public override Task Handle(ClientWantsToLogoffDto dto, IWebSocketConnection socket)
     {
-        throw new NotImplementedException();
+        
     }
 }
