6af19a27a07bf002a27148e88303ffd27a323d72
hoeeg79 <93433669+hoeeg79@users.noreply.github.com>
2024-04-30 13:53:27 +0200

    getting there!

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {SidebarComponent} from "./sidebar/sidebar.component";
 import {OverviewComponent} from "./overview/overview.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, OverviewComponent],
+  imports: [RouterOutlet, SidebarComponent, OverviewComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
