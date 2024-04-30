a4ae03a77c39537bd8aa9c8aad7bb5d75ffeb28b
DanKjaer <114471756+DanKjaer@users.noreply.github.com>
2024-04-30 10:34:54 +0200

    ClientTriggersAlarm made and tested.

--- backend/ws/client_event_handlers/ClientTriggersAlarm.cs
+++ backend/ws/client_event_handlers/ClientTriggersAlarm.cs
@@ -1,16 +1,34 @@
+using System.Text.Json;
+using api.transfer_models;
 using Fleck;
+using infrastructure.models;
 using lib;
+using service;
 
 namespace ws;
 
 public class ClientTriggersAlarmDto : BaseDto
 {
-    
+    public HistoryModel HistoryModel { get; set; }
 }
 public class ClientTriggersAlarm : BaseEventHandler<ClientTriggersAlarmDto>
 {
+    private readonly HistoryService _historyService;
+
+    public ClientTriggersAlarm(HistoryService historyService)
+    {
+        _historyService = historyService;
+    }
+
     public override Task Handle(ClientTriggersAlarmDto dto, IWebSocketConnection socket)
     {
-        throw new NotImplementedException();
+        HistoryModel loggedEvent = _historyService.CreateHistory(dto.HistoryModel);
+        var alarmTriggerDto = new ResponseDto()
+        {   
+            ResponseData = loggedEvent
+        };
+        var alarmTriggerToClient = JsonSerializer.Serialize(alarmTriggerDto);
+        socket.Send(alarmTriggerToClient);
+        return Task.CompletedTask;
     }
 }
