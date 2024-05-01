2406670e41ab135b6e636a4cf3f379bd8de24581
ShadySloth <pedant01@easv365.dk>
2024-05-01 10:58:32 +0200

    JWT ny og bedre

---
+++ backend/ws/RequireAuthentication.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Authentication;
+using Fleck;
+using lib;
+using service;
+
+namespace api;
+
+public class RequireAuthenticationAttribute : BaseEventFilter
+{
+    public override Task Handle<T>(IWebSocketConnection socket, T dto)
+    {
+        if (!StateService.GetClient(socket.ConnectionInfo.Id).IsAuthenticated)
+            throw new AuthenticationException("Client is not authenticated!");
+        return Task.CompletedTask;
+
+    }
+}
