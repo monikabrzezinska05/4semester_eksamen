83fae390f0b3f60672e81cdb8299781800c2c989
ShadySloth <pedant01@easv365.dk>
2024-05-01 08:58:53 +0200

    jeg er så træt

---
+++ backend/ws/JwtBearerHandler.cs
@@ -0,0 +1,36 @@
+﻿using service;
+
+namespace api;
+
+public class JwtBearerHandler
+{
+    private readonly ILogger<JwtBearerHandler> _logger;
+    private readonly RequestDelegate _next;
+
+    public JwtBearerHandler(ILogger<JwtBearerHandler> logger, RequestDelegate next)
+    {
+        _logger = logger;
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext http)
+    {
+        var jwtHelper = http.RequestServices.GetRequiredService<JwtService>();
+
+        try
+        {
+            var authHeader = http.Request.Headers.Authorization.FirstOrDefault();
+            if (authHeader != null && authHeader.StartsWith("Bearer "))
+            {
+                var token = authHeader.Split(" ")[1];
+                var data = jwtHelper.ValidateAndDecodeToken(token);
+                http.SetSessionData(data);
+            }
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Error extracting user from bearer token in authorization header.");
+        }
+        await _next.Invoke(http);
+    }
+}
