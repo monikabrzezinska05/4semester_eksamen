9b04b07f4f9a97ac26c420bc47a3f9a0842f3e0a
hoeeg79 <93433669+hoeeg79@users.noreply.github.com>
2024-05-02 09:01:29 +0200

    Change in historymodel

--- backend/infrastructure/models/HistoryModel.cs
+++ backend/infrastructure/models/HistoryModel.cs
@@ -4,7 +4,7 @@ public class HistoryModel
 {
     public int? HistoryId { get; set; }
     public string? UserEmail { get; set; }
-    public int UnitId { get; set; }
+    public Unit? Unit { get; set; }
     public DateTime Date { get; set; }
     public EventType EventTypeId { get; set; }
 }
--- backend/infrastructure/repositories/HistoryRepo.cs
+++ backend/infrastructure/repositories/HistoryRepo.cs
@@ -7,7 +7,7 @@ namespace infrastructure.repositories;
 public class HistoryRepo
 {
     private readonly NpgsqlDataSource _dataSource;
-    
+
     public HistoryRepo(NpgsqlDataSource dataSource)
     {
         _dataSource = dataSource;
@@ -18,11 +18,19 @@ public class HistoryRepo
         string sql = "";
         if (timePeriod != null)
         {
-            sql = "SELECT * FROM history WHERE history.date > @timePeriod";
+            sql =
+                "SELECT historyid, CONCAT(u.unitid, u.name, u.unittype, u.status) as \"Unit\", \"User\".name, eventtype, date\n" +
+                "FROM history\n" +
+                "JOIN public.unit u on u.unitid = history.unitid\n " +
+                "LEFT JOIN public.\"User\" on \"User\".mail = history.useremail WHERE history.date > @timePeriod";
         }
         else
         {
-            sql = "SELECT * FROM history";
+            sql =
+                "SELECT historyid, CONCAT(u.unitid, u.name, u.unittype, u.status) as \"Unit\", \"User\".name, eventtype, date\n" +
+                "FROM history\n" +
+                "JOIN public.unit u on u.unitid = history.unitid\n " +
+                "LEFT JOIN public.\"User\" on \"User\".mail = history.useremail";
         }
 
         using (var conn = _dataSource.OpenConnection())
@@ -33,10 +41,12 @@ public class HistoryRepo
 
     public HistoryModel CreateHistory(HistoryModel model)
     {
-        const string sql = "INSERT INTO history(useremail, unitId, date, eventtype) VALUES(@UserEmail, @unitid, @date, @EventTypeId) RETURNING *";
+        const string sql =
+            "INSERT INTO history(useremail, unitId, date, eventtype) VALUES(@UserEmail, @unitid, @date, @EventTypeId) RETURNING *";
         using (var conn = _dataSource.OpenConnection())
         {
-            var response = conn.QueryFirst<HistoryModel>(sql, new {model.UserEmail, model.UnitId, model.Date, model.EventTypeId});
+            var response = conn.QueryFirst<HistoryModel>(sql,
+                new { model.UserEmail, model.Unit.UnitId, model.Date, model.EventTypeId });
             return response;
         }
     }
