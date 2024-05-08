44d5a0d9da5c4524574d8d12221208cab28e33d1
DanKjaer <114471756+DanKjaer@users.noreply.github.com>
2024-05-08 11:29:36 +0200

    det grundliggende

--- backend/ws/client_event_handlers/ClientWantsToTurnOffAlarm.cs
+++ backend/ws/client_event_handlers/ClientWantsToTurnOffAlarm.cs
@@ -1,17 +1,37 @@
+using System.Text.Json;
+using api.transfer_models;
 using Fleck;
+using infrastructure.models;
 using lib;
+using service;
 
 namespace ws;
 
 public class ClientWantsToTurnOffAlarmDto: BaseDto
 {
-    
+    public HistoryModel historyModel { get; set; }
 }
 
 public class ClientWantsToTurnOffAlarm : BaseEventHandler<ClientWantsToTurnOffAlarmDto>
 {
+    private readonly HistoryService _historyService;
+    private readonly MQTTService _mqttService;
+
+    public ClientWantsToTurnOffAlarm(HistoryService historyService, MQTTService mqttService)
+    {
+        _historyService = historyService;
+        _mqttService = mqttService;
+    }
+
     public override Task Handle(ClientWantsToTurnOffAlarmDto dto, IWebSocketConnection socket)
     {
-        throw new NotImplementedException();
+        HistoryModel loggedEvent = _historyService.CreateHistory(dto.historyModel);
+        var turnOffAlarm = new ResponseDto()
+        {
+            ResponseData = loggedEvent
+        };
+        var turnOffAlarmToClient = JsonSerializer.Serialize(turnOffAlarm);
+        socket.Send(turnOffAlarmToClient);
+        return Task.CompletedTask;
     }
 }
