7926468fecdbecaa94ea3afb799f310f43bf3240
Lasse Høeg <93433669+hoeeg79@users.noreply.github.com>
2024-05-09 12:45:58 +0200

    vi kan se

--- frontend/src/services/state.service.ts
+++ frontend/src/services/state.service.ts
@@ -3,15 +3,15 @@ import {Unit, UnitType} from "../models/Unit";
 import {environment} from "../environments/environment";
 import {BaseDto, ServerOpensConnectionDto, ServerShowsHistoryDto} from "../models/BaseDto";
 import {HistoryModel} from "../models/HistoryModel";
+import {BehaviorSubject, combineLatest, map, Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class State {
-  units: Unit[] = [];
-  history: HistoryModel[] = [];
-
+  history$: BehaviorSubject<HistoryModel[]> = new BehaviorSubject<HistoryModel[]>([]);
+  units$: BehaviorSubject<Unit[]> = new BehaviorSubject<Unit[]>([]);
   ws: WebSocket = new WebSocket(environment.baseUrl);
 
   constructor() {
@@ -32,13 +32,32 @@ export class State {
   }
 
   ServerShowsHistory(dto: ServerShowsHistoryDto) {
-    this.history = [...this.history, ...dto.responseDto.responseData];
+    var current = this.history$.getValue();
+    this.history$.next([...current, ...dto.responseDto.responseData]);
   }
 
   ServerOpensConnection(dto: ServerOpensConnectionDto) {
-    console.log("First line: ", JSON.parse(JSON.stringify(dto)));
-    this.units = [...this.units, ...dto.responseDto.responseData];
-    console.log("done working on message thingy");
-    console.log(this.units);
+    var current = this.units$.getValue();
+    this.units$.next([...current, ...dto.responseDto.responseData]);
+  }
+
+  public getAllHistory(): Observable<HistoryModel[]> {
+    return this.history$.asObservable();
+  }
+
+  public getUnitHistory(unitId: number): Observable<HistoryModel[]> {
+    return this.history$.pipe(map((history) => history.filter((historyModel) => historyModel.unitId === unitId)));
+  }
+
+  public getDoors(): Observable<Unit[]> {
+    return this.units$.pipe(map((units) => units.filter((unit) => unit.unitTypeId === UnitType.Door)));
+  }
+
+  public getWindows() {
+    return this.units$.pipe(map((units) => units.filter((unit) => unit.unitTypeId === UnitType.Window)));
+  }
+
+  public getMotionSensor() {
+    return this.units$.pipe(map((units) => units.filter((unit) => unit.unitTypeId === UnitType.MotionSensor)));
   }
 }
