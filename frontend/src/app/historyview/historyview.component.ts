476ff7247c17b20c9027be9f24dabf90792a0dcd
DanKjaer <114471756+DanKjaer@users.noreply.github.com>
2024-05-23 10:24:00 +0200

    Search function

--- frontend/src/app/historyview/historyview.component.ts
+++ frontend/src/app/historyview/historyview.component.ts
@@ -1,27 +1,30 @@
 import {Component, OnInit} from '@angular/core';
 import {CommonModule} from "@angular/common";
-import { HistoryModel } from "../../models/HistoryModel";
-import {interval, map, Observable} from "rxjs";
+import {EventType, HistoryModel} from "../../models/HistoryModel";
+import {combineLatest, interval, map, Observable, startWith} from "rxjs";
 import {State} from "../../services/state.service";
 import {Status} from "../../models/Unit";
+import { FormControl, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-historyview',
   standalone: true,
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ],
   templateUrl: './historyview.component.html',
   styleUrl: './historyview.component.css'
 })
 export class HistoryviewComponent implements OnInit{
+  // - Used for search "searchControl = new FormControl('');"
   History$!: Observable<HistoryModel[]>;
   Status = Status;
 
   constructor(private state: State) {}
 
   ngOnInit(): void {
-    this.History$ = this.getHistoryObservable();
+    this.History$! =  this.getHistoryObservable();
   }
 
   private getHistoryObservable(): Observable<HistoryModel[]> {
@@ -36,8 +39,42 @@ export class HistoryviewComponent implements OnInit{
     );
   }
 
+
+
   protected readonly interval = interval;
   protected readonly Number = Number;
   protected readonly String = String;
 }
 
+
+/**
+ *  // Filtering logic
+ *   private filterLogs(historyItems: HistoryModel[], searchTerm: string): HistoryModel[] {
+ *     if(!searchTerm){
+ *       return historyItems;
+ *     }
+ *     const lowerCaseSearchTerm = searchTerm.toLowerCase();
+ *
+ *     return historyItems.filter(item => {
+ *     const eventTypeString = EventType[item.eventType];
+ *
+ *     const dateString = item.date.toISOString();
+ *
+ *     const unitString = item.unit.toString();
+ *
+ *     return (
+ *       eventTypeString.toLowerCase().includes(lowerCaseSearchTerm) ||
+ *         dateString.toLowerCase().includes(lowerCaseSearchTerm) ||
+ *         unitString.toLowerCase().includes(lowerCaseSearchTerm)
+ *     );
+ *     });
+ *   }
+ *
+ *   //ngOnInit
+ *       this.filteredHistory$ = combineLatest([
+ *       this.getHistoryObservable(),
+ *       this.searchControl.valueChanges.pipe(startWith(''))
+ *     ]).pipe(
+ *       map(([historyItems, searchTerm]) => this.filterLogs(historyItems, searchTerm!))
+ *     );
+ */
