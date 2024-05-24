59d84b8d75978c216013368bd8edcb538f667b84
hoeeg79 <93433669+hoeeg79@users.noreply.github.com>
2024-05-24 10:54:21 +0200

    test made, yml made but not tested yml

---
+++ e2e_test/e2e.js
@@ -0,0 +1,43 @@
+const {Selector} = require('testcafe');
+
+fixture(`Tests`).page("http://localhost:4200/login");
+
+test('Login and see the dashboard', async t => {
+    await t
+        .typeText('#usernameField', 'boobie@email.dk')
+        .typeText('#passwordField.ng-untouched.ng-pristine.ng-invalid', 'process.env.SECRET')
+        
+        .click("#loginButton.login-button")
+        .wait(2000)
+
+        .expect(Selector('#doorOverview').exists).ok()
+});
+
+test('Login and see the history page', async t => {
+    await t
+    .wait(2000)
+    .typeText('#usernameField', 'boobie@email.dk')
+    .typeText('#passwordField', 'password')
+    
+    .click("#loginButton.login-button")
+    
+    .wait(5000)
+    .click("#historyButton")
+    .wait(1000)
+
+    .expect(Selector('#historyHeadline').exists).ok()
+});
+
+test('Login and logout', async t => {
+    await t
+    .typeText('#usernameField', 'boobie@email.dk')
+    .typeText('#passwordField.ng-untouched.ng-pristine.ng-invalid', 'pasprocess.env.SECRET')
+    
+    .click("#loginButton.login-button")
+    .wait(3000)
+
+    .click("#logoutButton")
+    .wait(2000)
+
+    .expect(Selector('#loginButton').exists).ok()
+});
