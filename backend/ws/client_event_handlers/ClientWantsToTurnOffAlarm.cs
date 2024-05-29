f3b92cec7c50196b4f51ae2e2abbf04bcafc5e1d
ShadySloth <pedant01@easv365.dk>
2024-05-29 15:55:57 +0200

    slettet nogle cwl

--- backend/ws/client_event_handlers/ClientWantsToTurnOffAlarm.cs
+++ backend/ws/client_event_handlers/ClientWantsToTurnOffAlarm.cs
@@ -34,9 +34,7 @@ public class ClientWantsToTurnOffAlarm : BaseEventHandler<ClientWantsToTurnOffAl
         var unitsToUpdate = units.Where(u => u.UnitType != UnitType.MotionSensor).ToList();
         foreach (var unit in unitsToUpdate)
         {
-            Console.WriteLine("unit id" + unit.UnitId);
             dto.HistoryModel.UnitId = unit.UnitId;
-            Console.WriteLine("historymodel unitid" + dto.HistoryModel.UnitId);
             HistoryModel loggedEvent = _historyService.CreateHistory(dto.HistoryModel);
             loggedEvents.Add(loggedEvent);
         }
--- backend/ws/client_event_handlers/ClientWantsToTurnOnAlarm.cs
+++ backend/ws/client_event_handlers/ClientWantsToTurnOnAlarm.cs
@@ -33,9 +33,7 @@ public class ClientWantsToTurnOnAlarm : BaseEventHandler<ClientWantsToTurnOnAlar
         var unitsToUpdate = units.Where(u => u.UnitType != UnitType.MotionSensor).ToList();
         foreach (var unit in unitsToUpdate)
         {
-            Console.WriteLine("unit id" + unit.UnitId);
             dto.HistoryModel.UnitId = unit.UnitId;
-            Console.WriteLine("historymodel unitid" + dto.HistoryModel.UnitId);
             HistoryModel loggedEvent = _historyService.CreateHistory(dto.HistoryModel);
             loggedEvents.Add(loggedEvent);
         }
